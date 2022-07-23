430c180d895da4ccbea5e46f0ed119e2b979ffbf
kiruba094 <kirupakaran094@gmail.com>
2022-07-23 11:59:09 +0530

    add blob image

--- server/index.js
+++ server/index.js
@@ -21,17 +21,16 @@ app.get("/*", (req, res, next) => {
     // get post info
     const postId = req.query.id;
     const param=req.query.title;
-    const image=req.query.img;
     const post = getPostById(postId);
     if (!post) return res.status(404).send("Post not found");
 
     // inject meta tags
     htmlData = htmlData
-      // .replace("<title>React App</title>", `<title>${param}</title>`)
+      .replace("<title>React App</title>", `<title>${param}</title>`)
       .replace("__META_OG_TITLE__", param)
       .replace("__META_OG_DESCRIPTION__", post.description)
       .replace("__META_DESCRIPTION__", post.description)
-      .replace("__META_OG_IMAGE__", image)
+      .replace("__META_OG_IMAGE__", post.thumbnail)
       .replace("__META_OG_URL__", post.url);
     return res.send(htmlData);
   });
